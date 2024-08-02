3bdecdc367e88531c8064684a117bf3671a07b8e
Hazrat Ali <hazratali3711@gmail.com>
2024-08-02 17:01:05 +0600

    soft push

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -5,7 +5,6 @@ import { useMutation } from "@tanstack/react-query";
 export const useSignUp = (): any => {
   return useMutation({
     mutationFn: async (userSignUp: UserSignUp) => {
-      console.log("Inside service ", userSignUp);
       return await axios.post("http://localhost:5000/auth/signup", userSignUp);
     },
   });
